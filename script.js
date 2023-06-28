aada7752bcf002467fbe754fd017f456cd31799f
laurastefaniabirgaoanu <stefaniabirgaoanu@yahoo.com>
2023-06-28 15:23:07 +0300

    #update

--- script.js
+++ script.js
@@ -40,22 +40,69 @@
 // poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
 // poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
 
-const runOnce = function () {
-  console.log("This will never run again");
+// const runOnce = function () {
+//   console.log("This will never run again");
+// };
+// runOnce();
+// //IIFE
+// (function () {
+//   console.log("This will never run again");
+//   const isPrivate = 23;
+// })();
+
+// // console.log(isPrivate);
+// (() => console.log("This will also never run again"))();
+
+// {
+//   const isPrivate = 23;
+//   var notPrivate = 46;
+// }
+// // console.log(isPrivate);
+// console.log(notPrivate);
+//Closure
+// const secuteBooking = function () {
+//   let passengerCount = 0;
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+// const booker = secuteBooking();
+// booker();
+// booker();
+// booker();
+// console.dir(booker);
+
+//Example1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
 };
-runOnce();
-//IIFE
-(function () {
-  console.log("This will never run again");
-  const isPrivate = 23;
-})();
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+g();
+h();
+f();
+console.dir(f);
 
-// console.log(isPrivate);
-(() => console.log("This will also never run again"))();
+//Example 2
 
-{
-  const isPrivate = 23;
-  var notPrivate = 46;
-}
-// console.log(isPrivate);
-console.log(notPrivate);
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+  setTimeout(function () {
+    console.log(`We are all boarding all ${n} passengers `);
+    console.log(`We are 3 groups, each with ${perGroup} passengers `);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+const perGroup = 1000;
+boardPassengers(180, 3);
