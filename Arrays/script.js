5fe5d098cff7f32f44b1dee7bf75043c0a815273
laurastefaniabirgaoanu <stefaniabirgaoanu@yahoo.com>
2023-07-17 16:14:50 +0300

    #update

--- Arrays/script.js
+++ Arrays/script.js
@@ -161,3 +161,22 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // currenciesUnique.forEach(function (value, _, map) {
 //   console.log(`${value}: ${value}`);
 // });
+
+// DOgs project
+const checkDogs = function (dogJulia, dogKate) {
+  const dogsJuliaCorrected = dogJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  // dogJulia.slice(1, 3);
+  const dogs = dogsJuliaCorrected.concat(dogKate);
+  console.log(dogs);
+  dogs.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old.`);
+    } else {
+      console.log(`Dog ${i + 1} is still a puppy. `);
+    }
+  });
+};
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
