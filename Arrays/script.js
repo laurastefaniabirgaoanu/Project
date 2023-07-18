b0fe1b62c5ca3467216882d7ed8086ad1e410d72
laurastefaniabirgaoanu <stefaniabirgaoanu@yahoo.com>
2023-07-18 16:18:22 +0300

    #update

--- Arrays/script.js
+++ Arrays/script.js
@@ -72,6 +72,17 @@ const displayMovements = function (movements) {
   });
 };
 displayMovements(account1.movements);
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts); //STW
+console.log(accounts);
 
 // LECTURES
 
@@ -183,22 +194,22 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // Map Method
 
-const eurToUsd = 1.1;
-// const movementsUSD = movements.map(function (mov) {
-//   return mov * eurToUsd;
-// });
-const movementsUSD = movements.map(mov => mov * eurToUsd);
-console.log(movements);
-console.log(movementsUSD);
+// const eurToUsd = 1.1;
+// // const movementsUSD = movements.map(function (mov) {
+// //   return mov * eurToUsd;
+// // });
+// const movementsUSD = movements.map(mov => mov * eurToUsd);
+// console.log(movements);
+// console.log(movementsUSD);
 
-const movementsUSDfor = [];
-for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
-console.log(movementsUSDfor);
+// const movementsUSDfor = [];
+// for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+// console.log(movementsUSDfor);
 
-const movementsDescriptions = movements.map(
-  (mov, i) =>
-    `Movement ${i + 1}:You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
-      mov
-    )}`
-);
-console.log(movementsDescriptions);
+// const movementsDescriptions = movements.map(
+//   (mov, i) =>
+//     `Movement ${i + 1}:You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+//       mov
+//     )}`
+// );
+// console.log(movementsDescriptions);
